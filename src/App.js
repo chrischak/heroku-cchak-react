946e0f13841b808a44e156e49fd6b6b7dec091ed
cchak <chrischakct@gmail.com>
2017-06-21 16:19:00 -0400

    fixed the tag <body>-> <p>

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
-          <body>Hello, I am Chris first time putting an applications on to heroku
-          using react base javascripts</body>
+          <p>Hello, I am Chris first time putting an applications on to heroku
+          using react base javascripts</p>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
