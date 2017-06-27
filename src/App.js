f16d4f428d1b3f455eeb70483262e4de342ea471
cchak <chrischakct@gmail.com>
2017-06-26 23:34:22 -0400

    add tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Game from './Components/Game.js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Game />
       </div>
     );
   }
